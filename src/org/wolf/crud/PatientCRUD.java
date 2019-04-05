6768516f44131dc8f22c384d3e788891aa8a4c00
Anjali Malunjkar <anjalimalunjkar007@gmail.com>
2019-04-04 22:33:21 -0400

    Patient insert

--- src/org/wolf/crud/PatientCRUD.java
+++ src/org/wolf/crud/PatientCRUD.java
@@ -1,11 +1,11 @@
 package org.wolf.crud;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.wolf.baseclasses.Patient;
+import org.wolf.config.*;
 
 class PatientMapper implements RowMapper {
 	public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -24,41 +24,60 @@ class PatientMapper implements RowMapper {
 public class PatientCRUD {
 
 	//Returns the  information for the patient with given Id
-	public Patient viewPatient(Integer id){
+	public static Patient viewPatient(Integer id){
 		return null;
 	}
 	
 	//Returns information of all patients
-	public ArrayList<Patient> viewPatient(){
+	public static ArrayList<Patient> viewPatients(){
 		return null;
 	}
 	
 	//Returns list of patient information searched by given name
 	//String matching LIKE query 
-	public ArrayList<Patient> getPatientIdByName(String name){
+	public static ArrayList<Patient> getPatientIdByName(String name){
 		return null;	
 	}
 	
 	//Patient Info. search by ssn
-	public Patient getPatientIdBySSN(String ssn){
+	public static Patient getPatientIdBySSN(String ssn){
 		return null;	
 	}
 	
 	//Returns the newly created patient Id
 	//If fails returns null
-	public Integer insertPatient(String name, String ssn, String dob, String gender, String pno, String address){
-		return null;
+	public static Integer insertPatient(String name, String ssn, String dob, String gender, String pno, String address){
+		try
+	    {
+			Connection conn = DatabaseConnection.getConnection();
+			
+		    Statement st = conn.createStatement();
+		    st.executeUpdate("INSERT INTO Patient(name, address, phone_no, gender, ssn, date_of_birth) " +
+		                       "VALUES ('"+ name +"', '"+ address+"', '"+pno+"', '"+gender+"', '"+ssn+"', '"+dob+"')");
+		    ResultSet rs = st.executeQuery("SELECT patient_id FROM Patient ORDER BY patient_id DESC LIMIT  1");
+		    int patient_id = 0;
+		    while(rs.next()) {
+		    	patient_id = rs.getInt("patient_id");
+		    }
+		    
+		    return patient_id;
+	    }
+	    catch (SQLException ex)
+	    {
+	    	System.err.println(ex.getMessage());
+	    	return null;
+	    }
 	}
 	
 	//Returns true if the update was successful else false
 	//Updates the record with this new information using the ID.
-	public Boolean updatePatient(Integer id, String name, String ssn, String dob, String gender, String pno, String address){
+	public static Boolean updatePatient(Integer id, String name, String ssn, String dob, String gender, String pno, String address){
 		return null;
 	}
 	
 	// Deletes the patient information  using the patient Id
 	//Returns true if successful else false
-	public Boolean deletePatient(Integer id){
+	public static Boolean deletePatient(Integer id){
 		return null;
 	}
 }
